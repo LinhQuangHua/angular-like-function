c8140d3d6b3558db56d87059171b6f757605b3b6
LinhQuangHua <hualinhquang281@gmail.com>
2021-03-15 22:46:02 +0700

    Initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,29 @@
+import { Component, VERSION } from '@angular/core';
+
+@Component({
+  selector: 'my-app',
+  templateUrl: './app.component.html',
+  styleUrls: [ './app.component.css' ]
+})
+export class AppComponent  {
+  name = "Bài tập chương 2";
+  test = " "
+  like = 45;
+  state = 'off';
+
+  like_function() {
+    if(this.state === 'off')
+    {
+      this.like +=1;
+      this.state = 'on';
+      this.test = "Hứa Linh Quang đã thích ảnh này!";
+    }
+    else if(this.state === 'on')
+    {
+      this.like -=1;
+      this.state = 'off';
+      this.test = " ";
+    }
+  }
+
+}
